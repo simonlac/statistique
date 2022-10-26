5a17ae4586ef44037cbd0575a04c3de327173cdf
simonlac <101816652+simonlac@users.noreply.github.com>
2022-10-26 10:58:07 -0400

    Add files via upload

---
+++ Classes_Statiques/Classes_Statiques/Classes_Statiques/Classe_Statique.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Statiques
+{
+    static class Classe_Statique
+    {
+        // Membres statiques
+        public static double PI = 3.14;
+        //
+        public static int somme (int p_a,int p_b)
+        {
+            return p_a + p_b;
+        }
+    }
+}
---
+++ Classes_Statiques/Classes_Statiques/Classes_Statiques/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Statiques
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Classe_Statique.PI);
+            Console.WriteLine(Classe_Statique.somme(4, 8).ToString());
+            // Erreur : On ne peut pas instancier un objet d'une classe statique
+            //Classe_Statique cs1 = new Classe_Statique(); 
+            Console.ReadKey();
+        }
+    }
+}
